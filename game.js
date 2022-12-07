accb5dd207ddbd79baf38675331648f0b4d5212d
georgiagrayland <georgiamayeva@live.co.uk>
2022-12-07 11:31:54 +0000

    add question array outline and fill in some questions text content

--- game.js
+++ game.js
@@ -9,3 +9,86 @@ let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
+
+let questions = [
+    {//question 1
+        question: "What is the Capital City of the USA?",
+        choice1: "New York City",
+        choice2: "Washington D.C.",
+        choice3: "Toronto",
+        choice4: "Budapest",
+        answer: 2,
+    },
+    {
+        question: "Which is the largest Ocean on Earth?",
+        choice1: "The Pacific Ocean",
+        choice2: "The Atlantic Ocean",
+        choice3: "The Southern Ocean",
+        choice4: "The Indian Ocean",
+        answer: 1,
+    },
+    {
+        question: "Where are the Andes Mountains?",
+        choice1: "Europe",
+        choice2: "Australia",
+        choice3: "Asia",
+        choice4: "South America",
+        answer: 4,
+    },
+    {
+        question: "Which is the largest waterfall on Earth?",
+        choice1: "Angel Falls",
+        choice2: "Niagra Falls",
+        choice3: "Victoria Falls",
+        choice4: "Sutherland Falls",
+        answer: 3,
+    },
+    {//question 5
+        question: "What is the highest country (by elevation) in the world?",
+        choice1: "Bhutan",
+        choice2: "China",
+        choice3: "Peru",
+        choice4: "Switzerland",
+        answer: 1,
+    },
+    {//question 6 
+        question: "Which is the second most populous country in the world?",
+        choice1: "Russia",
+        choice2: "India",
+        choice3: "Brazil",
+        choice4: "Germany",
+        answer: 2,
+    },
+    {//question 7
+        question: "Which sea separates Europe and Africa?",
+        choice1: "The Baltic Sea",
+        choice2: "The Dead Sea",
+        choice3: "The North Sea",
+        choice4: "The Mediterranean Sea",
+        answer: 4,
+    },
+    {
+        question: "What is the capital city of the USA?",
+        choice1: "New York City",
+        choice2: "Washington D.C.",
+        choice3: "Toronto",
+        choice4: "Budapest",
+        answer: 2,
+    },
+    {
+        question: "What is the capital city of the USA?",
+        choice1: "New York City",
+        choice2: "Washington D.C.",
+        choice3: "Toronto",
+        choice4: "Budapest",
+        answer: 2,
+    },
+    {//question 10 
+        question: "What is the capital city of the USA?",
+        choice1: "New York City",
+        choice2: "Washington D.C.",
+        choice3: "Toronto",
+        choice4: "Budapest",
+        answer: 2,
+    },
+]
