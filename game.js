cf9c10ec26ee8bf15122ac6f691b698c5bc20255
georgiagrayland <georgiamayeva@live.co.uk>
2022-12-07 11:16:30 +0000

    assign consts to elements

---
+++ game.js
@@ -0,0 +1,5 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progress-bar-full');
