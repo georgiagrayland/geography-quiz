90f5f7eac3b2123205626ae39730d17348d2a41c
georgiagrayland <georgiamayeva@live.co.uk>
2022-12-07 16:43:57 +0000

    add all missing script tags in html files

--- highscores.js
+++ highscores.js
@@ -1,3 +1,7 @@
 
 const highScoresList = document.querySelector('#high-scores-list');
-const highScores = JSON.parse
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+highScoresList.innerHTML = highScores.map(score => {
+    return ` <li class="high-score">${score.name} - ${score.score}</li>`
+}).join('')
