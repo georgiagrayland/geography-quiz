212e9a97d3d40f9144c7c535a259a573c2307d6d
georgiagrayland <georgiamayeva@live.co.uk>
2023-01-25 16:01:32 +0000

    edit text animaitons and rules text

--- assets/js/start.js
+++ assets/js/start.js
@@ -1,7 +1,28 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+//Animation for Title Text
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.5em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
 
 //Showing the rules on click on the button on homepage
 const rulesList = document.getElementById('rules');
     rulesList.addEventListener('click', function(){
-   document.getElementById("show-rules").innerHTML = `<p id="clicked-rules">There will be 10 questions to answer. <br>You will have 20 seconds to answer each question <br>The color will show if you have answered correct or not. 
+   document.getElementById("show-rules").innerHTML = `<p id="clicked-rules">There will be 10 questions to answer. <br>There is a timer to see how fast you can complete the quiz. <br>The color will show if you have answered correct or not. 
     <br> You will be told your final score at the end of the game.</p>`;
 });
+
